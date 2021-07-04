bf2380c950f8ef14b0afac927bfa8529c88af824
laurapalma <lau.palma9617@gmail.com>
2021-07-03 19:13:56 -0500

    Función isValid

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import validator from './validator.js';
 
-console.log(validator);
+// eslint-disable-next-line no-console
+//console.log(validator.maskify());
+//validator.isValid("123");
+document.getElementById("btnValidar").addEventListener("click", () => {
+    //alert("hola");
+    validator.isValid(document.getElementById("tarjetaDeCredito").value);
+    }
+
+);
