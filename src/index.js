b86ab65e5bae98757d3be9c6ead45df2cc14bbe9
laurapalma <lau.palma9617@gmail.com>
2021-07-08 11:09:31 -0500

    Cambios en html, css y js

--- src/index.js
+++ src/index.js
@@ -1,35 +1,37 @@
 import validator from './validator.js';
 
+    //Interacción de la segunda pantalla     
+    //let pantalla2 = document.getElementById("pantalla2");
+    document.getElementById('product-container').style.display = "block";
+    document.getElementById('content').style.display = "none";
+    document.getElementById('compra').style.display = "none";
+    
+    let iconCart=document.getElementById("iconCart");
+    iconCart.addEventListener("click",mostrarpantalla);
+
+    function mostrarpantalla(){
+        //let pantalla1 = document.getElementById("pantalla1");
+        document.getElementById('product-container').style.display = "none";
+        document.getElementById('content').style.display = "block";        
+        document.getElementById('compra').style.display = "block";
+    }
+
+    let enviar=document.getElementById("enviar");
+    enviar.addEventListener("click",mostrarpantalla1);
+
+    function mostrarpantalla1(){
+       //let pantalla2 = document.getElementById("pantalla2");
+       document.getElementById('product-container').style.display = "block";
+       document.getElementById('content').style.display = "none";
+    }
+
 // eslint-disable-next-line no-console
 //console.log(validator.maskify());
 //validator.isValid("123");
 document.getElementById("btnValidar").addEventListener("click", () => {
-    //alert("hola");
-    validator.isValid(document.getElementById("tarjetaDeCredito").value);
+    
+    let resultado = validator.isValid(document.getElementById("tarjetaDeCredito").value);
+    alert(resultado);
     }
 
 );
-
-//function mostrarpantalla(){
-//    let pantalla = document.getElementById("pantalla");
-//    document.getElementById('container').style.display = "";
-//    document.getElementById('product-container').style.display = "none";
-//    document.getElementById('content').style.display = "block";
-//    document.getElementById('social-media').style.display = "";
-//}
-
-//function footer(){
-    //let pantalla = document.getElementById("pantalla");
-    //document.getElementById('container').style.display = "";
-    //document.getElementById('product-container').style.display = "none";
-    //document.getElementById('content').style.display = "block";
-    //document.getElementById('social-media').style.display = "";
-//}
-
-//function enviar(){
-//    let enviarpantalla = document.getElementById("enviarpantalla");
-//    document.getElementById('container').style.display = "";
-//    document.getElementById('product-container').style.display = "block";
-//    document.getElementById('content').style.display = "none";
-//    document.getElementById('social-media').style.display = "";
-//}
